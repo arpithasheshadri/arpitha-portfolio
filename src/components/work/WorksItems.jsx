81e9ce3c7c497dc6de068526c3a90972ca59ed4e
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-01-24 15:15:52 -0500

    modified links in work section

--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -5,7 +5,7 @@ const WorksItems = ({item}) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className="work__img"/>
             <h3 className="work__title">{item.title}</h3>
-            <a href={item.link != "" ? item.link : "#"} className="work__button">
+            <a href={item.link != "" ? item.link : "#"} target="_blank" className="work__button">
                 Description <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
         </div>
