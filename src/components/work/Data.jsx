fc3e8ca9dfc41abfff75f24745bb15a5a72563ac
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-04-13 15:08:07 -0400

    added new projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -5,56 +5,72 @@ import Work4 from '../../assets/work4.webp';
 import Work5 from '../../assets/work5.jpeg';
 import Work6 from '../../assets/work6.jpeg';
 import Work7 from '../../assets/work7.png';
+import Work10 from '../../assets/work10.png';
+import Work9 from '../../assets/work9.png';
 
 export const projectsData = [
     {
         id: 1,
+        image: Work10,
+        title: 'CloudTier: Terraform Orchestrated RESTful Service',
+        category: 'Web',
+        link: 'https://github.com/arpithasheshadri/webapp'
+    },
+    {
+        id: 2,
         image: Work3,
         title: 'Voyage',
         category: 'Web',
         link: 'https://github.com/arpithasheshadri/Voyage'
     },
     {
-        id: 2,
+        id: 3,
         image: Work1,
-        title: 'FunDo',
+        title: 'KeepFlow: Dynamic Note-Keeping',
         category: 'Web',
         link: 'https://github.com/arpithasheshadri/FunDoApp'
     },
     {
-        id: 3,
+        id: 4,
         image: Work2,
         title: 'Book Store Management System',
         category: 'Web',
         link: 'https://github.com/arpithasheshadri/BookStore'
     },
     {
-        id: 4,
+        id: 5,
         image: Work7,
         title: 'Intrusion Detection System For Wireless Network',
         category: 'Data Science',
         link: ''
     },
     {
-        id: 5,
+        id: 6,
         image: Work4,
         title: 'End-to-End Encryption For Fitness Trackers',
         category: 'Cybersecurity',
         link: 'http://paper.ijcsns.org/07_book/202206/20220610.pdf'
     },
     {
-        id: 6,
+        id: 7,
         image: Work6,
         title: 'Templar Threat Intelligence Project',
         category: 'Web',
         link: 'https://github.com/bhatappu11/alienvault'
     },
     {
-        id: 7,
+        id: 8,
         image: Work5,
         title: 'Charity Blockchain',
         category: 'Web',
         link: 'https://github.com/bhatappu11/webpage-for-charity-using-reactjs'
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: 'Weather Wave',
+        category: 'Web',
+        link: 'https://github.com/arpithasheshadri/SupplyFrameCodingChallenge'
     }
 ]
 
